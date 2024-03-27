656ded157f3fd87af04c705ae50f6f6f4a3b2b11
dongguo <291320608@qq.com>
2024-03-28 07:58:39 +0800

    payment8081

---
+++ cloud-provider-payment8001/src/main/java/com/dongguo/cloud/Payment8001Application.java
@@ -0,0 +1,15 @@
+package com.dongguo.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@MapperScan("com.dongguo.cloud.mapper") //import tk.mybatis.spring.annotation.MapperScan;
+public class Payment8001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Payment8001Application.class, args);
+	}
+
+}
